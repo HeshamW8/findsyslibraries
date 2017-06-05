37c192e96e4374186e39654769cf1fd4c1912f03
HeshamW8 <heshamwardany@gmail.com>
2017-06-05 19:14:33 +0200

    API call added, responds appropriately

--- api/README.md
+++ api/README.md
@@ -9,3 +9,7 @@ certain ruby gem uses on a certain platform.
 1. Run the following command in the terminal to populate the database with gem dependencies from the gemdataset.json
 
 mongoimport --db gemLookup --collection gems --drop --file gemdataset.json
+
+2. To make an API call visit localhost:3000 and send two parameters, the gem name and the platform name
+
+example API call : http://localhost:3000/?name=sqlite3&&platform=apt
--- api/app.js
+++ api/app.js
@@ -11,16 +11,15 @@ var server = http.createServer(app);
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
 
 // Connection URL
 var dbURL = 'mongodb://localhost:27017/gemLookup';
 
 // db instance
 var mongodb;
+var gemsCollection;
 
 // Use connect method to connect to the server
 MongoClient.connect(dbURL, function (err, db) {
@@ -28,8 +27,30 @@ MongoClient.connect(dbURL, function (err, db) {
     console.log("Connected successfully to database");
 
     mongodb = db;
+    gemsCollection = db.collection('gems');
 });
+app.get('/', function (req, res) {
+    var gemName = req.query.name;
+    var platform = req.query.platform;
 
+    if (gemName && platform) {
+        gemsCollection.findOne({ name: gemName }, function (err, doc) {
+            if (doc) {
+                console.log(doc);
+                var requiredPlatform = doc.platforms[platform];
+                if (requiredPlatform) {
+                    res.send(requiredPlatform);
+                } else {
+                    res.send('no information about this platform');
+                }
+            } else {
+                res.send('gem not available');
+            }
+        });
+    } else {
+        res.send('invalid api call');
+    }
+});
 
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
